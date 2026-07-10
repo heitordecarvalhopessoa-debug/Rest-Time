2160127ab2f46af675501fafbeb1fc9a0ebdee4b
Heitor de Carvalho Pessoa <heitordecarvalhopessoa@gmail.com>
2026-07-10 18:24:53 -0300

    Create storage.ts

---
+++ src/ts/storage.ts
@@ -0,0 +1,41 @@
+interface UserData {
+    name: string;
+    totalTimeSpent: number;
+}
+
+interface DataStorageInterface {
+    loadUserData: () => UserData | null;
+    saveUserData: (data: UserData) => void;
+    clearUserData: () => void;
+}
+
+const STORAGE_KEY: string = 'rest_time_data';
+
+const DataStorage: DataStorageInterface = {
+    loadUserData: (): UserData | null => {
+        const data = localStorage.getItem(STORAGE_KEY);
+        return data ? JSON.parse(data) : null;
+    },
+
+    saveUserData: (data: UserData): void => {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+    },
+
+    clearUserData: (): void => {
+        localStorage.removeItem(STORAGE_KEY);
+    }
+};
+
+declare global {
+    interface Window {
+        DataStorage: DataStorageInterface;
+        GameVersion?: {
+            current: string;
+            init: () => void;
+        };
+    }
+}
+
+window.DataStorage = DataStorage;
+
+export {};
