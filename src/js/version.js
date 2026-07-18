a68864aeacb45024560dd554595c33122d69a46f
Heitor de Carvalho Pessoa <heitordecarvalhopessoa@gmail.com>
2026-07-18 15:49:11 -0300

    Update version.js

--- src/js/version.js
+++ src/js/version.js
@@ -1,5 +1,5 @@
 const GameVersion = {
-    current: "v0.4.0",
+    current: "v0.4.2",
     init: function() {
         const display = document.getElementById('version-display');
         if (display) {
